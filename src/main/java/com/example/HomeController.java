a827ab422627ffb74c6f86b0af92c3566581015e
leeejjju <dlwlswn02027@gmail.com>
2022-11-30 17:16:21 +0900

    221130

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,2 +1,31 @@
-package com.example;public class HomeController {
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @RequestMapping("classlist")
+    public String list(Model model){
+        List<String> list = new ArrayList<String>();
+        list.add("Yeji Song");
+        list.add("Hanmin Park");
+        list.add("Jinju Lee");
+
+        model.addAttribute("classlist", list);
+        String msg = "동뱅지킴이 List";
+        model.addAttribute("title", msg);
+
+        return "board/list";
+    }
 }
+
